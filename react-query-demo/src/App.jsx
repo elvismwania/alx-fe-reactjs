89f4dd4995ddab8651667c5b85a7b3632305dd14
Elvis Mwania. <elvismwania@gmail.com>
2025-03-12 23:25:54 +0300

    Set up React Query in App.js and implemented PostsComponent

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,14 +1,17 @@
-import PostsComponent from './PostsComponent'
+// src/App.js
+
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './PostsComponent';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div style={{ maxWidth: '800px', margin: '0 auto', padding: '2rem' }}>
-      <h1 style={{ textAlign: 'center', marginBottom: '2rem' }}>
-        React Query Demo
-      </h1>
+    <QueryClientProvider client={queryClient}>
       <PostsComponent />
-    </div>
-  )
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
